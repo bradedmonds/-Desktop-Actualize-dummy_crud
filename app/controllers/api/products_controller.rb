2686bd49ed114b8e6a505172e9ee5a9376b81523
Brad Edmonds <bcedmonds0417@gmail.com>
2021-02-28 10:15:46 -0600

    make index route. make index action, make index view

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,13 @@
 class Api::ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    render "index.json.jb"
+  end
+
+  # def create
+  #   @product = Product.new(
+      
+  #   )
+  #   render "show.json.jb"
+  # end
 end
